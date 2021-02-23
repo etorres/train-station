ae83da84eb157a74dd3d8bcf5f492c45894c3d35
Erik Torres <erik.torres@lifullconnect.com>
2021-02-23 12:04:07 +0100

    :recycle: Extract common decoding logic

--- libs/models-circe/src/main/scala/circe.scala
+++ libs/models-circe/src/main/scala/circe.scala
@@ -8,72 +8,49 @@ import cats.implicits._
 import io.circe._
 
 import java.time.Instant
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 object circe {
   trait EventJsonProtocol {
-    implicit val eventIdDecoder: Decoder[EventId] = (cursor: HCursor) =>
-      cursor
-        .downField("eventId")
-        .as[String]
-        .map(EventId.fromString)
-        .fold(
-          _.asLeft, {
-            case Left(constructorError) =>
-              DecodingFailure(
-                s"Failed to decode: ${cursor.value.toString}, with error: ${constructorError.error}",
-                List.empty
-              ).asLeft
-            case Right(eventId) => eventId.asRight
-          }
-        )
+    implicit val eventIdDecoder: Decoder[EventId] =
+      fieldDecoder[EventId]("eventId", EventId.fromString)
 
     implicit val eventIdEncoder: Encoder[EventId] = (eventId: EventId) =>
       Json.obj(("eventId", Json.fromString(eventId.unEventId.value)))
   }
 
   trait TrainJsonProtocol {
-    implicit val trainIdDecoder: Decoder[TrainId] = (cursor: HCursor) =>
-      cursor
-        .downField("trainId")
-        .as[String]
-        .map(TrainId.fromString)
-        .fold(
-          _.asLeft, {
-            case Left(constructorError) =>
-              DecodingFailure(
-                s"Failed to decode: ${cursor.value.toString}, with error: ${constructorError.error}",
-                List.empty
-              ).asLeft
-            case Right(trainId) => trainId.asRight
-          }
-        )
+    implicit val trainIdDecoder: Decoder[TrainId] =
+      fieldDecoder[TrainId]("trainId", TrainId.fromString)
 
     implicit val trainIdEncoder: Encoder[TrainId] = (trainId: TrainId) =>
       Json.obj(("trainId", Json.fromString(trainId.unTrainId.value)))
   }
 
   trait MomentJsonProtocol {
+    implicit def momentDecoder[A <: Moment.When]: Decoder[Moment[A]] =
+      fieldDecoder[Moment[A]]("moment", str => Try(Instant.parse(str)).map(Moment[A]).toEither)
+
     implicit def momentEncoder[A <: Moment.When]: Encoder[Moment[A]] =
       (moment: Moment[A]) => Json.obj(("moment", Json.fromString(moment.unMoment.toString)))
-
-    implicit def momentDecoder[A <: Moment.When]: Decoder[Moment[A]] =
-      (cursor: HCursor) =>
-        cursor
-          .downField("moment")
-          .as[String]
-          .map(str => Try(Instant.parse(str)))
-          .fold(
-            _.asLeft,
-            a =>
-              a match {
-                case Failure(exception) =>
-                  DecodingFailure(
-                    s"Failed to decode: ${cursor.value.toString}, with error: ${exception.getMessage}",
-                    List()
-                  ).asLeft
-                case Success(instant) => Moment[A](instant).asRight
-              }
-          )
   }
+
+  private[this] def fieldDecoder[A](field: String, fA: String => Either[_ <: Throwable, A]) =
+    new Decoder[A] {
+      override def apply(cursor: HCursor): Decoder.Result[A] =
+        cursor
+          .downField(field)
+          .as[String]
+          .map(fA)
+          .fold(
+            _.asLeft, {
+              case Left(constructorError) =>
+                DecodingFailure(
+                  s"Failed to decode: ${cursor.value.toString}, with error: ${constructorError.getMessage}",
+                  List.empty
+                ).asLeft
+              case Right(value) => value.asRight
+            }
+          )
+    }
 }
--- libs/models/src/main/scala/error.scala
+++ libs/models/src/main/scala/error.scala
@@ -6,6 +6,8 @@ object error {
   sealed trait TrainStationError extends NoStackTrace
 
   object TrainStationError {
-    final case class InvalidParameter(error: String) extends TrainStationError
+    final case class InvalidParameter(error: String) extends TrainStationError {
+      override def getMessage: String = error
+    }
   }
 }
