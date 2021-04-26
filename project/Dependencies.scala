419a60ae7e65f9c0942022cf42e1cd61565e2c11
Scala Steward <me@scala-steward.org>
2021-04-26 20:37:02 +0200

    Update fs2 to 1.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -209,7 +209,7 @@ trait Tapir {
 
   private[this] val modelVersion = "1.4.4"
   private[this] val sharedCoreVersion = "1.2.2"
-  private[this] val sharedFs2Version = "1.1.1"
+  private[this] val sharedFs2Version = "1.2.3"
   private[this] val tapirVersion = "0.18.0-M7"
 
   val sttpModelCore = modelOrganization %% "core" % modelVersion
