f4ee633185dbce56ed1bd68af5b04ecb717e8bb2
Erik Torres <erik.torres@lifullconnect.com>
2021-07-31 21:45:42 +0200

    :arrow_up: Upgrade tapir-fs2 to 1.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -205,13 +205,12 @@ trait Tapir {
   private[this] val tapirOrganization = "com.softwaremill.sttp.tapir"
 
   private[this] val modelVersion = "1.4.9"
-  private[this] val sharedCoreVersion = "1.2.5"
-  private[this] val sharedFs2Version = "1.1.1"
+  private[this] val sharedVersion = "1.2.5"
   private[this] val tapirVersion = "0.18.1"
 
   val sttpModelCore = modelOrganization %% "core" % modelVersion
-  val sttpSharedCore = sharedOrganization %% "core" % sharedCoreVersion
-  val sttpSharedFs2 = sharedOrganization %% "fs2" % sharedFs2Version
+  val sttpSharedCore = sharedOrganization %% "core" % sharedVersion
+  val sttpSharedFs2 = sharedOrganization %% "fs2-ce2" % sharedVersion
   val tapirCore = tapirOrganization %% "tapir-core" % tapirVersion
   val tapirHttp4sServer = tapirOrganization %% "tapir-http4s-server" % tapirVersion
   val tapirJsonCirce = tapirOrganization %% "tapir-json-circe" % tapirVersion
