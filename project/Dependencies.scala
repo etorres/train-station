1b12d7192fa255d2ec4638fe95b8760a462c67ae
Scala Steward <me@scala-steward.org>
2021-10-21 18:26:58 +0200

    Update shared:core, shared:fs2 to 1.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -204,7 +204,7 @@ trait Tapir {
   private[this] val tapirOrganization = "com.softwaremill.sttp.tapir"
 
   private[this] val modelVersion = "1.4.15"
-  private[this] val sharedVersion = "1.2.6"
+  private[this] val sharedVersion = "1.2.7"
   private[this] val tapirVersion = "0.19.0-M4"
 
   val sttpModelCore = modelOrganization %% "core" % modelVersion
