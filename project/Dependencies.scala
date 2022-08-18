24527de5b7095cd6848c14b58cf5c1a434fa874e
Scala Steward <scala_steward@virtuslab.com>
2022-08-18 14:24:13 +0000

    Update tapir-core, tapir-http4s-server, ... to 1.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -207,7 +207,7 @@ trait Tapir {
 
   private[this] val modelVersion = "1.5.0"
   private[this] val sharedVersion = "1.3.7"
-  private[this] val tapirVersion = "0.20.2"
+  private[this] val tapirVersion = "1.0.5"
 
   val sttpModelCore = modelOrganization %% "core" % modelVersion
   val sttpSharedFs2 = sharedOrganization %% "fs2" % sharedVersion
