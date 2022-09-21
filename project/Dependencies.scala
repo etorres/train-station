04af6b62d415070bb01a8e9cd0a0e1c65e083140
Erik Torres Serrano <etserrano@gmail.com>
2022-09-21 14:32:22 +0200

    Upgrade shapeless to version 2.3.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -194,7 +194,7 @@ trait ScalaLang {
 trait Shapeless {
   private[this] val organization = "com.chuusai"
 
-  private[this] val version = "2.3.9"
+  private[this] val version = "2.3.10"
 
   val shapeless = organization %% "shapeless" % version
 }
