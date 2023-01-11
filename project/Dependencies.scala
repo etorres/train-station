8b255853862c4f42c87f427b100ee7704d2e553a
Scala Steward <scala_steward@virtuslab.com>
2023-01-11 14:48:45 +0000

    Update openapi-circe-yaml, openapi-model to 0.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -214,7 +214,7 @@ trait Tapir {
   private[this] val sharedOrganization = "com.softwaremill.sttp.shared"
   private[this] val tapirOrganization = "com.softwaremill.sttp.tapir"
 
-  private[this] val apispecVersion = "0.3.1"
+  private[this] val apispecVersion = "0.3.2"
   private[this] val modelVersion = "1.5.3"
   private[this] val sharedVersion = "1.3.12"
   private[this] val tapirVersion = "1.2.4"
