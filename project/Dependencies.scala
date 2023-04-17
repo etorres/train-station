a23e6c76a774fdcc97d0082f072edd6c03877fb7
Scala Steward <scala_steward@virtuslab.com>
2023-04-17 14:45:33 +0000

    Update cats-effect, cats-effect-kernel to 3.4.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ trait Cats {
   private[this] val organization = "org.typelevel"
 
   private[this] val catsVersion = "2.9.0"
-  private[this] val catsEffectVersion = "3.4.8"
+  private[this] val catsEffectVersion = "3.4.9"
   private[this] val kittensVersion = "3.0.0"
 
   val catsCore = organization %% "cats-core" % catsVersion
