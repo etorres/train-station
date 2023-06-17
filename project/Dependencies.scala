c00f2e6bbac5a9b10a0a02dc28647083408a6d5c
Erik Torres Serrano <etserrano@gmail.com>
2023-06-17 10:50:32 +0200

    Update http4s to 0.23.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -113,7 +113,7 @@ trait Hikari {
 trait Http4s {
   private[this] val organization = "org.http4s"
 
-  private[this] val version = "0.23.19"
+  private[this] val version = "0.23.20"
 
   val http4sCirce = organization %% "http4s-circe" % version
   val http4sCore = organization %% "http4s-core" % version
