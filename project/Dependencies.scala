359969ef2bb59593d5b8e9440b0e6fd48b750195
Scala Steward <scala_steward@virtuslab.com>
2023-07-26 15:30:08 +0000

    Update tapir-core, tapir-http4s-server, ... to 1.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -217,7 +217,7 @@ trait Tapir {
   private[this] val apispecVersion = "0.4.0"
   private[this] val modelVersion = "1.7.1"
   private[this] val sharedVersion = "1.3.16"
-  private[this] val tapirVersion = "1.6.0"
+  private[this] val tapirVersion = "1.6.3"
 
   val openApiModel = apispecOrganization %% "openapi-model" % apispecVersion
   val openApiCirceYaml = apispecOrganization %% "openapi-circe-yaml" % apispecVersion
