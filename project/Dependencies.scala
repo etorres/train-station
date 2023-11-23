908a511b053cba0ebcb78366af7900bc13e1da03
Scala Steward <scala_steward@virtuslab.com>
2023-11-23 17:05:58 +0000

    Update fs2 to 1.3.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -216,7 +216,7 @@ trait Tapir {
 
   private[this] val apispecVersion = "0.4.0"
   private[this] val modelVersion = "1.7.6"
-  private[this] val sharedVersion = "1.3.16"
+  private[this] val sharedVersion = "1.3.17"
   private[this] val tapirVersion = "1.6.0"
 
   val openApiModel = apispecOrganization %% "openapi-model" % apispecVersion
